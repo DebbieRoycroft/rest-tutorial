206c926106bd10c27e2148ed500bb44d15c6ae12
DebbieRoycroft <debbie.roycroft@server.fake>
2015-04-14 11:53:14 +0100

    Add custom json converters for LocalDateTime

--- timetable/src/main/java/timetable/model/Event.java
+++ timetable/src/main/java/timetable/model/Event.java
@@ -2,10 +2,17 @@ package timetable.model;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 public class Event {
 	private Long id;
 	private String title;
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
 	private LocalDateTime startTime;
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
 	private LocalDateTime endTime;
 	
 	public Event(Long id, String title, 
