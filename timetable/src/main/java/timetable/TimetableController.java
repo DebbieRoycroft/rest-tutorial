a20c344d58760f7cbb36eabbe28f3151b12c1ace
DebbieRoycroft <DebbieRoycroft@users.noreply.github.com>
2015-04-22 19:32:39 +0100

    Add in JPA and extend Event class

--- timetable/src/main/java/timetable/TimetableController.java
+++ timetable/src/main/java/timetable/TimetableController.java
@@ -1,25 +1,26 @@
 package timetable;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import timetable.model.Event;
+import timetable.model.EventRepository;
 
 @Controller
 @RequestMapping("/timetable")
 public class TimetableController {
 
+	@Autowired
+	EventRepository eventRepo;
+		
 	@RequestMapping(method=RequestMethod.GET)
-	public @ResponseBody List<Event> getEvents(){
-		List<Event> events = new ArrayList<>();
-		events.add(new Event(1L,"Yoga"));
-		events.add(new Event(2L,"Boxercise"));
-		return events;
+	public @ResponseBody Collection<Event> getEvents(){
+		return eventRepo.findAll();
 		
 	}
 }
--- timetable/src/main/java/timetable/model/Event.java
+++ timetable/src/main/java/timetable/model/Event.java
@@ -1,12 +1,26 @@
 package timetable.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Event {
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String title;
+	private Integer day;
+	private String startTime;
+	private String endTime;
+
+	public Event() {}
 	
-	public Event(Long id, String title) {
-		this.id = id;
+	public Event(String title, Integer day, String startTime, String endTime) {
 		this.title = title;
+		this.day = day;
+		this.startTime = startTime;
+		this.endTime = endTime;
 	}
 
 	public Long getId() {
@@ -15,6 +29,18 @@ public class Event {
 
 	public String getTitle() {
 		return title;
+	}
+
+	public Integer getDay() {
+		return day;
+	}
+	
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
 	}
 
 }
